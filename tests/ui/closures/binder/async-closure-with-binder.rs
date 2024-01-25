42b046b43d2bb809597510f57bd89f4e747f0fcf
Michael Goulet <michael@errs.io>
2024-01-25 03:50:23 +0000

    bless tests

--- tests/ui/closures/binder/async-closure-with-binder.rs
+++ tests/ui/closures/binder/async-closure-with-binder.rs
@@ -2,7 +2,5 @@
 #![feature(closure_lifetime_binder)]
 #![feature(async_closure)]
 fn main() {
-    for<'a> async || ();
-    //~^ ERROR `for<...>` binders on `async` closures are not currently supported
-    //~^^ ERROR implicit types in closure signatures are forbidden when `for<...>` is present
+    for<'a> async || -> () {};
 }
