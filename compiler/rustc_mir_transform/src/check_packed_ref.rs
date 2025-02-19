4be4ade56d9b2fa8bb717c6ad1af975f0fc38178
Michael Goulet <michael@errs.io>
2025-02-19 03:36:03 +0000

    Don't manually track SourceInfo for CheckedPackedRef

--- compiler/rustc_mir_transform/src/check_packed_ref.rs
+++ compiler/rustc_mir_transform/src/check_packed_ref.rs
@@ -10,8 +10,7 @@ pub(super) struct CheckPackedRef;
 impl<'tcx> crate::MirLint<'tcx> for CheckPackedRef {
     fn run_lint(&self, tcx: TyCtxt<'tcx>, body: &Body<'tcx>) {
         let typing_env = body.typing_env(tcx);
-        let source_info = SourceInfo::outermost(body.span);
-        let mut checker = PackedRefChecker { body, tcx, typing_env, source_info };
+        let mut checker = PackedRefChecker { body, tcx, typing_env };
         checker.visit_body(body);
     }
 }
@@ -20,25 +19,13 @@ struct PackedRefChecker<'a, 'tcx> {
     body: &'a Body<'tcx>,
     tcx: TyCtxt<'tcx>,
     typing_env: ty::TypingEnv<'tcx>,
-    source_info: SourceInfo,
 }
 
 impl<'tcx> Visitor<'tcx> for PackedRefChecker<'_, 'tcx> {
-    fn visit_terminator(&mut self, terminator: &Terminator<'tcx>, location: Location) {
-        // Make sure we know where in the MIR we are.
-        self.source_info = terminator.source_info;
-        self.super_terminator(terminator, location);
-    }
-
-    fn visit_statement(&mut self, statement: &Statement<'tcx>, location: Location) {
-        // Make sure we know where in the MIR we are.
-        self.source_info = statement.source_info;
-        self.super_statement(statement, location);
-    }
-
-    fn visit_place(&mut self, place: &Place<'tcx>, context: PlaceContext, _location: Location) {
+    fn visit_place(&mut self, place: &Place<'tcx>, context: PlaceContext, location: Location) {
         if context.is_borrow() && util::is_disaligned(self.tcx, self.body, self.typing_env, *place)
         {
+            let span = self.body.source_info(location).span;
             let def_id = self.body.source.instance.def_id();
             if let Some(impl_def_id) = self.tcx.impl_of_method(def_id)
                 && self.tcx.is_builtin_derived(impl_def_id)
@@ -46,9 +33,9 @@ impl<'tcx> Visitor<'tcx> for PackedRefChecker<'_, 'tcx> {
                 // If we ever reach here it means that the generated derive
                 // code is somehow doing an unaligned reference, which it
                 // shouldn't do.
-                span_bug!(self.source_info.span, "builtin derive created an unaligned reference");
+                span_bug!(span, "builtin derive created an unaligned reference");
             } else {
-                self.tcx.dcx().emit_err(errors::UnalignedPackedRef { span: self.source_info.span });
+                self.tcx.dcx().emit_err(errors::UnalignedPackedRef { span });
             }
         }
     }
